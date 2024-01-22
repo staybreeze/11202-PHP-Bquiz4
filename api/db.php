de7b5e0729e380187003aafb67e59c9d361cb550
staybreeze <wunshengliao@gmail.com>
2024-01-22 10:54:57 +0800

    feat:建立後台頁尾版權管理頁面

--- api/db.php
+++ api/db.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Taipei");
 session_start();
 class DB{
 
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db15";
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=db03";
     protected $pdo;
     protected $table;
     
@@ -141,8 +141,7 @@ function to($url){
     header("location:$url");
 }
 
-
-
+$Bottom=new DB('bottom');
 
 
 ?>
