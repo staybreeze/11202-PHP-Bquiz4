5a730e1b579126f1436f014d57961062b4342be8
staybreeze <wunshengliao@gmail.com>
2024-01-22 11:09:03 +0800

    feat:完成後台頁尾版權編輯功能

--- back/bot.php
+++ back/bot.php
@@ -1,11 +1,22 @@
+<?php
+if(!empty($_POST)){
+    $Bottom->save(['bottom'=>$_POST['bottom'],'id'=>1]);
+}
+?>
+
 <h2 class="ct">編輯頁尾版權區</h2>
-<form action="?" method="post">
+<form action="?do=bot" method="post">
     <!-- table.all>tr>td.tt+td.pp>input:text -->
     <table class="all">
         <tr>
             <td class="tt">頁尾宣告內容</td>
-            <td class="pp"><input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>"></td>
+            <td class="pp">
+                <input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>">
+            </td>
         </tr>
     </table>
-    <div class="ct"><input type="submit" value="編輯"><input type="reset" value="重置"></div>
+    <div class="ct">
+        <input type="submit" value="編輯">
+        <input type="reset" value="重置">
+    </div>
 </form>
