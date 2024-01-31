0af4a5c6009e1cc7489711e315833bb0f553d490
staybreeze <wunshengliao@gmail.com>
2024-01-31 10:23:42 +0800

    feat:完成管理員列表及編輯按鈕功能

---
+++ api/del.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once "db.php";
+$db=new DB($_POST['table']);
+$db->del($_POST['id']);
--- back/admin.php
+++ back/admin.php
@@ -8,23 +8,23 @@
         <th class="tt ct">管理</th>
     </tr>
     <?php
-    $rows=$Admin->all();
-    foreach($rows as $row){
+    $rows = $Admin->all();
+    foreach ($rows as $row) {
     ?>
-    <tr>
-        <td class="pp ct"><?=$row['acc'];?></td>
-        <td class="pp ct"><?=str_repeat("*",strlen($row['pw']));?></td>
-        <td class="pp ct">
-            <?php
-                if($row['acc']=='admin'){
+        <tr>
+            <td class="pp ct"><?= $row['acc']; ?></td>
+            <td class="pp ct"><?= str_repeat("*", strlen($row['pw'])); ?></td>
+            <td class="pp ct">
+                <?php
+                if ($row['acc'] == 'admin') {
                     echo "此帳號為最高權限";
-                }else{
+                } else {
                     echo "<button onclick='location.href=&#39;?do=edit_admin&id={$row['id']}&#39;'>修改</button>";
-                    echo "<button onclick=''>刪除</button>";
+                    echo "<button onclick='del(&#39;admin&#39;,{$row['id']})'>刪除</button>";
                 }
-            ?>
-        </td>
-    </tr>
+                ?>
+            </td>
+        </tr>
     <?php
     }
     ?>
@@ -32,3 +32,15 @@
 <div class="ct">
     <button onclick="location.href='index.php'">返回</button>
 </div>
+
+<script>
+    function del(table, id) {
+        $.post("./api/del.php", {
+            table,
+            id
+        }, () => {
+            location.reload();
+
+        })
+    }
+</script>
