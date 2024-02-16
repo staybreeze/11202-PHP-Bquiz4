bbf2383093de00456e44fce3060042f4de855485
staybreeze <wunshengliao@gmail.com>
2024-02-16 14:10:50 +0800

    feat:製作詳細內容頁及加上我要購買按鈕

--- front/main.php
+++ front/main.php
@@ -5,15 +5,44 @@ $nav="";
 
 if($type==0){
     $nav="全部商品";
+    $goods=$Goods->all(['sh'=>1]);
 }else{
     $t=$Type->find($type);
     if($t['big_id']==0){
         $nav=$t['name'];
+        $goods=$Goods->all(['sh'=>1,'big'=>$t['id']]);
     }else{
         $b=$Type->find($t['big_id']);
         $nav=$b['name'].">".$t['name'];
+        $goods=$Goods->all(['sh'=>1,'mid'=>$t['id']]);
     }
 }
 ?>
 
 <h2><?=$nav;?></h2>
+
+<?php
+
+foreach($goods as $good){
+?>
+<div class="item">
+    <div class="img">
+    <a href="?do=detail&id=<?=$good['id'];?>">
+  
+        <img src="./img/<?=$good['img'];?>" style="width:200px;height:150px">
+
+    </a>
+    </div>
+    <div class="info">
+        <div class='ct tt'><?=$good['name'];?></div>
+        <div>價格:<?=$good['price'];?>
+        <img src="./icon/0402.jpg" style="float:right">
+    </div>
+        <div>規格:<?=$good['spec'];?></div>
+        <div>簡介：<?=mb_substr($good['intro'],0,25);?>...</div>
+    </div>
+</div>
+<?php
+
+}
+?>
